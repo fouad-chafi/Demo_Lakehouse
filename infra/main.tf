0583097579be62c863bb9848574bdfc80a17ebbf
fouad-chafi <chafi.fouad@gmail.com>
2026-01-28 00:43:07 +0100

    main adapt

--- infra/main.tf
+++ infra/main.tf
@@ -1,13 +1,4 @@
 terraform {
-  backend "s3" {
-    bucket         = "tfstate-0749"
-    key            = "aws-demo-datalake/terraform.tfstate"
-    region         = "eu-central-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
-terraform {
   required_version = ">= 1.5.0"
 
   required_providers {
@@ -15,6 +6,14 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.30"
     }
+  }
+
+  backend "s3" {
+    bucket         = "tfstate-0749"
+    key            = "aws-demo-datalake/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
   }
 }
 
@@ -25,7 +24,5 @@ provider "aws" {
 data "aws_caller_identity" "current" {}
 
 locals {
-  bucket_name = var.s3_bucket_name != "" ? var.s3_bucket_name : "${var.project_name}-${data.aws_caller_identity.current.account_id}"
-  raw_prefix  = "raw/big_mac_index/"
-  athena_prefix = "athena-results/"
-}
+  bucket_name    = var.s3_bucket_name != "" ? var.s3_bucket_name : "${var.project_name}-${data.aws_caller_identity.current.account_id}"
+  raw_prefix     = "raw_
